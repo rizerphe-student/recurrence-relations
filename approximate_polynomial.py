f6ee6c2212ecca0b5cb1bc9fae1357f8e107cfc2
rizerphe <44440399+rizerphe@users.noreply.github.com>
2022-12-22 18:30:35 +0200

    Preserving duplicate roots

--- approximate_polynomial.py
+++ approximate_polynomial.py
@@ -17,31 +17,43 @@ def is_zero(value: float, precision: float) -> bool:
     return abs(value) < precision
 
 
-def remove_duplicates(roots: set[float], precision: float) -> set[float]:
-    """Remove duplicate roots"""
-    return {round(root, int(math.log10(1 / precision))) for root in roots}
-
-
-def binary_search(terms: list[float], start: float, end: float, precision: float) -> float:
+def binary_search(
+    terms: list[float], start: float, end: float, precision: float, max_iter: int = 100
+) -> float | None:
     """Find all solutions to a polynomial"""
-    while not is_zero(start - end, precision):
-        sign = evaluate_polynomial(terms, start) * evaluate_polynomial(
-            terms, (start + end) / 2)
-        if sign <= 0:
-            end = (start + end) / 2
-        else:
+    if is_zero(evaluate_polynomial(terms, start), precision):
+        return start
+    if is_zero(evaluate_polynomial(terms, end), precision):
+        return end
+    if evaluate_polynomial(terms, start) * evaluate_polynomial(terms, end) > 0:
+        return None
+    i = 0
+    mid_value = 0.0
+    while not (is_zero(start - end, precision) and is_zero(mid_value, precision)):
+        start_value = evaluate_polynomial(terms, start)
+        mid_value = evaluate_polynomial(terms, (start + end) / 2)
+        if start_value * mid_value > 0:
             start = (start + end) / 2
+        else:
+            end = (start + end) / 2
+
+        i += 1
+        if i > max_iter:
+            return None
     return (start + end) / 2
 
-def approximate_polynomial(terms: list[float], start: float, end: float, precision: float) -> set[float]:
+
+def approximate_polynomial(
+    terms: list[float], start: float, end: float, precision: float
+) -> list[float]:
     """Approximate all solutions to a polynomial"""
     # Solve a linear equation
     if len(terms) == 2:
-        return {-terms[1] / terms[0]}
+        return [-terms[1] / terms[0]]
 
     # Find all extremas
     extremas = approximate_polynomial(find_derivative(terms), start, end, precision)
-    points_of_interest = extremas.union({start, end})
+    points_of_interest = extremas + [start, end]
     print("extremas", terms, extremas)
     print("points_of_interest", terms, points_of_interest)
 
@@ -51,31 +63,28 @@ def approximate_polynomial(terms: list[float], start: float, end: float, precisi
     prev_loc = start
     for extreme in sorted(points_of_interest):
         new_sign = evaluate_polynomial(terms, extreme)
-        if new_sign * sign <= 0:
-            ranges.append((prev_loc, extreme))
-        sign = new_sign
+        ranges.append((prev_loc, extreme))
         prev_loc = extreme
+        sign = new_sign
     print("ranges", terms, ranges)
 
     # Find all roots in each range
-    roots = set()
-    for start, end in ranges:
-        roots.add(binary_search(terms, start, end, precision))
-    print("raw roots", terms, roots)
-    roots = roots.union(
-        {
-            extreme
-            for extreme in extremas
-            if is_zero(evaluate_polynomial(terms, extreme), precision)
-        }
-    )
-    roots = remove_duplicates(roots, precision)
+    roots = []
+    for start_, end_ in ranges:
+        if is_zero(start_ - end_, precision) and is_zero(
+            evaluate_polynomial(terms, (start_ + end_) / 2), precision
+        ):
+            roots.append((start_ + end_) / 2)
+            continue
+        candidate = binary_search(terms, start_, end_, precision)
+        if candidate is not None:
+            roots.append(candidate)
     print("roots", terms, roots)
     return roots
 
-print(approximate_polynomial([1, 0, -1, 0], -100, 100, 0.0001))
-# print(approximate_polynomial([1, 0, -1, 0, 0], -100, 100, 0.0001))
-# print(approximate_polynomial([1, 2], -100, 100, 0.0001))
+
+# print(approximate_polynomial([1, -8, 21, -20, 5], -100, 100, 0.0001))
+print(approximate_polynomial([1, 0, -1, 0, 0], -100, 100, 0.000001))
 # print(
 #     approximate_polynomial(
 #         [1, 69, -2646, 20820, 130344, -2579424, 12394496, 19568640], -100, 100, 0.0001
